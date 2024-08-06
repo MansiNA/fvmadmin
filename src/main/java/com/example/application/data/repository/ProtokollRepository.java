c0b54454184a4a67b659f8fafcda90d12f4d04d8
MansiNA <mansikavathiya1998@gmail.com>
2024-08-06 15:20:16 +0200

    added showlogs in MailboxConfigView

--- src/main/java/com/example/application/data/repository/ProtokollRepository.java
+++ src/main/java/com/example/application/data/repository/ProtokollRepository.java
@@ -4,6 +4,9 @@ import com.example.application.data.entity.Protokoll;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProtokollRepository extends JpaRepository<Protokoll, Long> {
+    List<Protokoll> findAllByOrderByZeitpunktDesc();
 }
