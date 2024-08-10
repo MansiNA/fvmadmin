84486f2faa75a689de1b9290b0902a92153c1be5
michael <m.quaschny@dbuss.de>
2024-08-10 10:17:27 +0200

    Mail angepasst

--- src/main/java/com/example/application/service/EmailService.java
+++ src/main/java/com/example/application/service/EmailService.java
@@ -2,4 +2,5 @@ package com.example.application.service;
 
 public interface EmailService {
     void sendSimpleMessage(String to, String subject, String text);
+    void sendAttachMessage(String to, String subject, String text, String attachement);
 }
--- src/main/java/com/example/application/service/EmailServiceImpl.java
+++ src/main/java/com/example/application/service/EmailServiceImpl.java
@@ -1,8 +1,15 @@
 package com.example.application.service;
 
+import jakarta.mail.Message;
+import jakarta.mail.internet.InternetAddress;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Service;
+
+import java.io.File;
 
 @Service
 public class EmailServiceImpl implements EmailService {
@@ -17,11 +24,39 @@ public class EmailServiceImpl implements EmailService {
     @Override
     public void sendSimpleMessage(String to, String subject, String text){
         SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom("test@test.de");
+        message.setFrom("m.quaschny@dataport.de");
         message.setTo(to);
         message.setSubject(subject);
         message.setText(text);
         emailSender.send(message);
+    }
+
+    @Override
+    public void sendAttachMessage(String to, String subject, String text, String attachement) {
+        MimeMessagePreparator preparator = mimeMessage -> {
+            mimeMessage.setRecipient(Message.RecipientType.TO, new InternetAddress(to));
+            mimeMessage.setFrom(new InternetAddress("demo@thexpertdev.com"));
+            mimeMessage.setSubject(subject);
+            mimeMessage.setText(text);
+
+            //String attachment = "test.csv";
+            try{
+
+                FileSystemResource file = new FileSystemResource(new File(attachement));
+                if (file.exists()){
+                    MimeMessageHelper helper= new MimeMessageHelper(mimeMessage,true);
+                    helper.addAttachment(file.getFilename(), file);
+                    helper.setText("",true);
+                } else {
+                    System.out.println("File not Found: " + attachement);
+                }
+            } catch (Exception ex){
+                ex.printStackTrace();
+            }
+
+        };
+
+        emailSender.send (preparator);
     }
 
 }
